8f9d90e27afb7a0d6938e243a93e90d4994c2036
sotpotatis <albin@albins.website>
2022-09-19 20:24:22 +0200

    Update services and scripts

--- systemd_services/start_menu_api.sh
+++ systemd_services/start_menu_api.sh
@@ -4,4 +4,4 @@
 #Feel free to change it to your install position.
 echo "Running menu API..."
 cd /home/ubuntu/eatery_menu/EateryCacher || exit 1
-gunicorn create_server:app -b "127.0.0.1:5000" #Run the server (using gunicorn)
+gunicorn create_server:app -b "127.0.0.1:80" #Run the server (using gunicorn)
