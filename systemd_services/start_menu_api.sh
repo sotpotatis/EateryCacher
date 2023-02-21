37bc7611f1480acfea5cdf286849a5912d10d5a3
sotpotatis <albin@albins.website>
2023-02-21 19:22:58 +0100

    Update default server URL

--- systemd_services/start_menu_api.sh
+++ systemd_services/start_menu_api.sh
@@ -5,4 +5,4 @@
 #Feel free to change it to your install position.
 echo "Running menu API..."
 cd /home/ubuntu/eatery_menu/EateryCacher || exit 1
-gunicorn create_server:app -b "127.0.0.1:80" #Run the server (using gunicorn)
+gunicorn create_server:app -b "0.0.0.0:80" #Run the server (using gunicorn)
